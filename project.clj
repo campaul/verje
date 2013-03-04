85ada24bfe8908816d98828317add264516b8537
Cameron Paul <cpaul37@gmail.com>
2013-03-04 00:27:49 -0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject verje "0.1.0-SNAPSHOT"
+  :description "A text based persistent world written in Clojure."
+  :url "https://github.com/campaul/verje"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main verje.core)
---
+++ src/verje/core.clj
@@ -0,0 +1,7 @@
+(ns verje.core
+  (:gen-class))
+
+(defn -main
+  "Start verje in client or server mode."
+  [& args]
+  (println "Hello, World!"))

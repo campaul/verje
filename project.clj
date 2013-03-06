4458ed76aaf8fee23597dafa37c0f5104a74dc81
Cameron Paul <cpaul37@gmail.com>
2013-03-06 13:16:59 -0800

    Added clojure-lanterna to project

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-beta7"]]
+                 [aleph "0.3.0-beta7"]
+                 [clojure-lanterna "0.9.2"]]
   :main verje.core)
